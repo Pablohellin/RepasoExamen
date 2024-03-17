9f865feb3d7bcc0775aa7a295e10abce6788e062
pablohellin <phellin9@alu.ucam.edu>
2024-03-17 18:29:28 +0100

    commit2

--- Item.java
+++ Item.java
@@ -1,6 +1,18 @@
 public class Item {
     public String nombre;
     public double precio;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
     public Item(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio; 
